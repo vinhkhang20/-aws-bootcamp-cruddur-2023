07b4a8db45e3731c4fe6215804b23c7ae5f1f662
Khang Nguyen <vinhkhang20@gmail.com>
2023-03-03 23:36:30 +0000

    update honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -21,7 +21,6 @@ from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-from opentelemetry.sdk.trace.export import ConsoleSpanProcessor, SimpleSpanProcessor
 
 
 # HoneyComb ------------------
@@ -29,11 +28,6 @@ from opentelemetry.sdk.trace.export import ConsoleSpanProcessor, SimpleSpanProce
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
-
-# Show this in the logs within the backend-flask app (STDOUT)
-simple_processor = SimpleSpanProcessor(ConsoleSpanExporter())
-provider.add_span_processor(simple_processor)
-
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
