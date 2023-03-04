f3fa1a08b4438087290a1cd7ec3c8175483c4500
Khang Nguyen <vinhkhang20@gmail.com>
2023-03-04 21:26:13 +0000

    update signin error message + create user

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -23,12 +23,12 @@ from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 # X-ray ----------------------
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
+# from aws_xray_sdk.core import xray_recorder
+# from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
-xray_url = os.getenv("AWS_XRAY_URL")
-xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
+# xray_url = os.getenv("AWS_XRAY_URL")
+# xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
+# XRayMiddleware(app, xray_recorder)
 
 # HoneyComb ------------------
 # Initialize tracing and an exporter that can send data to Honeycomb
